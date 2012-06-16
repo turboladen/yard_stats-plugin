e10aab00e7724f67198bf6e22ccf9bb7a7bc35fd
Steve Loveless <steve.loveless@gmail.com>
2012-06-15 21:51:40 -0700

    Defined #getIconFileName to stop getting an undefined method error on that.  Yes, it's a hack for now.

--- models/yard_stats_action.rb
+++ models/yard_stats_action.rb
@@ -1,6 +1,12 @@
 class YardStatsAction < Jenkins::Model::HealthReportingAction
   display_name 'Yard Stats'
   icon 'gear.png'
+
+  def getIconFileName
+    # Defining so I don't get an error saying this isn't defined.  Not sure why
+    # I'm getting that error, since this method is defined in Action, which is
+    # included in HealthReportingAction.
+  end
 end
 
 Jenkins::Plugin.instance.register_extension(YardStatsAction)
